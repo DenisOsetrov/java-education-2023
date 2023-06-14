c2554180da88dad3dbff6bd40f288b7ef4384c40
DenisOsetrov <dvosetrov@gmail.com>
2023-06-14 11:22:24 +0300

    hw-4 part 2

--- src/homework/hw_4/part2/Main.java
+++ src/homework/hw_4/part2/Main.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 //        Створити ArrayList зі словами на 15-20 елементів.
 //        - відсортувати його за алфавітом.
@@ -14,7 +15,7 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Word> wordArrayList = new ArrayList<>();
+        List<Word> wordArrayList = new ArrayList<>();
         wordArrayList.add(new Word("protected"));
         wordArrayList.add(new Word("predicate"));
         wordArrayList.add(new Word("runner"));
@@ -35,6 +36,18 @@ public class Main {
         // Функція Word::getWord повертає рядок (слово), за яким потрібно порівнювати.
         // Внутрішньо, метод Comparator.comparing використовує String.compareTo для порівняння рядків за алфавітом.
         wordArrayList.sort(Comparator.comparing(Word::getWord));
+
+        // 2 варіант
+        wordArrayList.sort((o1, o2) -> o1.getWord().compareTo(o2.getWord()));
+
+        // Якщо я працюю з типом String (List<String> wordArrayList = new ArrayList<>();)
+        //                  а не Word    List<Word> wordArrayList = new ArrayList<>();
+                //wordArrayList.sort((o1, o2) -> o1.compareTo(o2));  // сортує за алфавітом об'єкти типу <String>
+                //wordArrayList.sort(String::compareTo);             // сортує за алфавітом об'єкти типу <String>
+                //wordArrayList.sort(Comparator.naturalOrder());     // сортує стрінгу за натуральним порядком
+
+        // Сортуваня від Я-А
+        // wordArrayList.sort(Comparator.comparing(Word::getWord, Comparator.reverseOrder()));
 
         // Сортування за довжиною - від меншого до більшого
         // wordArrayList.sort((o1, o2) -> o1.getWord().length() - o2.getWord().length());
