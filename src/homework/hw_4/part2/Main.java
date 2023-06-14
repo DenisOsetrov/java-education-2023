cd94aa2446a1c98b8d60c205ad265f17be02a66f
DenisOsetrov <dvosetrov@gmail.com>
2023-06-14 10:26:58 +0300

    hw-4 part 2

--- src/homework/hw_4/part2/Main.java
+++ src/homework/hw_4/part2/Main.java
@@ -1,6 +1,47 @@
 package homework.hw_4.part2;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
 //        Створити ArrayList зі словами на 15-20 елементів.
 //        - відсортувати його за алфавітом.
+
+@Data
+@AllArgsConstructor
 public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Word> wordArrayList = new ArrayList<>();
+        wordArrayList.add(new Word("protected"));
+        wordArrayList.add(new Word("predicate"));
+        wordArrayList.add(new Word("runner"));
+        wordArrayList.add(new Word("shape"));
+        wordArrayList.add(new Word("remove"));
+        wordArrayList.add(new Word("react"));
+        wordArrayList.add(new Word("choose"));
+        wordArrayList.add(new Word("increase"));
+        wordArrayList.add(new Word("flight"));
+        wordArrayList.add(new Word("equals"));
+        wordArrayList.add(new Word("event"));
+        wordArrayList.add(new Word("continuation"));
+        wordArrayList.add(new Word("business"));
+        wordArrayList.add(new Word("reducer"));
+        wordArrayList.add(new Word("additional"));
+
+        // // Метод Comparator.comparing приймає функцію, яка повертає значення для порівняння.
+        // Функція Word::getWord повертає рядок (слово), за яким потрібно порівнювати.
+        // Внутрішньо, метод Comparator.comparing використовує String.compareTo для порівняння рядків за алфавітом.
+        wordArrayList.sort(Comparator.comparing(Word::getWord));
+
+        // Сортування за довжиною - від меншого до більшого
+        // wordArrayList.sort((o1, o2) -> o1.getWord().length() - o2.getWord().length());
+
+        for (Word word : wordArrayList) {
+            System.out.println(word); // Сортує за алфавітом: additional, business, choose.
+        }
+
+    }
 }
---
+++ src/homework/hw_4/part2/Word.java
@@ -0,0 +1,11 @@
+package homework.hw_4.part2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Word {
+    private String word;
+}
